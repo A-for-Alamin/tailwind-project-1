6972056baf651116ed964eccae2addabbc6b3fa4
Al amin <alaminmridha2004@gmail.com>
2024-07-05 18:52:25 +0600

    Tailwind Project 1 V0.1

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: 'class',
+
+
+  content: ["./public/index.html","./public/news.html"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
